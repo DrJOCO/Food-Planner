d460fc9dee97baa80f05fd05efb2549e839facaf
jchengmd <joncheng5@gmail.com>
2026-06-24 20:47:46 -0700

    Configure Firebase sync

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,8 @@
-window.FOOD_PLANNER_FIREBASE_CONFIG = null;
+window.FOOD_PLANNER_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDoM_A1nsCuL3g8p-MoP76cyjryMi--TmY",
+  authDomain: "chloecookbook.firebaseapp.com",
+  projectId: "chloecookbook",
+  storageBucket: "chloecookbook.firebasestorage.app",
+  messagingSenderId: "1068534763770",
+  appId: "1:1068534763770:web:72367df6deecbdd5b3b943",
+};
